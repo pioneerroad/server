a1547e6a49cbfcd2a046566bd1a265da5b5eee12
Matt <developer@pioneerroad.com.au>
2015-05-06 16:07:28 +1000

    added sql dump with no geog for states table

---
+++ data/states_no_geog.sql
@@ -0,0 +1,33 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: states; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY states (label, name, "createdAt", "updatedAt") FROM stdin;
+NSW	New South Wales	2015-05-06 08:12:18.516367+10	2015-05-06 08:12:18.516367+10
+VIC	Victoria	2015-05-06 08:12:18.516367+10	2015-05-06 08:12:18.516367+10
+QLD	Queensland	2015-05-06 08:12:18.516367+10	2015-05-06 08:12:18.516367+10
+SA	South Australia	2015-05-06 08:12:18.516367+10	2015-05-06 08:12:18.516367+10
+WA	Western Australia	2015-05-06 08:12:18.516367+10	2015-05-06 08:12:18.516367+10
+TAS	Tasmania	2015-05-06 08:12:18.516367+10	2015-05-06 08:12:18.516367+10
+NT	Northern Territory	2015-05-06 08:12:18.516367+10	2015-05-06 08:12:18.516367+10
+ACT	Australian Capital Territory	2015-05-06 08:12:18.516367+10	2015-05-06 08:12:18.516367+10
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
