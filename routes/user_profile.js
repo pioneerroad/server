be5a2adebc944dccd2370c469126e5a38d542c79
Matt <developer@pioneerroad.com.au>
2015-09-25 17:22:54 +1000

    Updated background-photo endpoint

--- routes/user_profile.js
+++ routes/user_profile.js
@@ -219,9 +219,6 @@ module.exports = function(app, userSockets, s3, router) {
     /** Endpoint for user profile background photo upload **/
     router.put('/user/:uid/profile/update/background-photo', [jwtAuth, accessAdmin, accessOwner, accessVerify],
         function (req, res) {
-            var uid = req.params.uid;
-            var image = req.files.image; image.fileNameBase = image.name.slice(0, image.name.indexOf('.')); //Store the filename without extension
-            var cropDimensions = {"width":req.body.width, "height":req.body.height, "x":req.body.x, "y":req.body.y}; // Read cropping dimensions from post data
             var processedImageSizes = {
                 "large": {
                     "width": 1280,
@@ -236,73 +233,22 @@ module.exports = function(app, userSockets, s3, router) {
                     "height": 107
                 }
             };
-            console.log(image);
-            var fileStream = fs.createReadStream(image.path);
 
-            var params = require('../config/awsS3config.js').userContent.images;
-            params.ContentType = image.mimetype;
-            var imageUploadS3 = require('../controllers/imageUploadS3');
-
-            imageUploadS3.getImageSize(image.path)
-                .then(function (imageSize) {
-                    processedImageSizes.original = {"width":imageSize.width, "height":imageSize.height}; // Add size of uploaded images to defaultImageSize array
-                    return imageUploadS3.getUser(uid, User)
-                        .then(function (userData) {
-                            var profilePhotoData = {}; // Initialise profilePhoto object
-                            /* Iterate through the array of sizes and process the image using the parameters for each on */
-                            for (var key in processedImageSizes) {
-                                if (processedImageSizes.hasOwnProperty(key)) {
-                                    (function(imageSize) {
-                                        if (imageSize == 'original') { // If this is the original image size, then just upload it
-                                            params.Key = 'profile-photos/'+userData.id+'/background-photos/'+image.name;
-                                            params.Body = fs.createReadStream(image.path);
-                                            return imageUploadS3.s3Upload(params)
-                                                .then(function(uploadData) {
-                                                    // Success
-                                                })
-                                                .catch(function(error) {
-                                                    res.status(400).json('Error: '+error);
-                                                });
-                                        } else {
-                                            gm(fileStream, image.name)
-                                                .crop(cropDimensions.width, cropDimensions.height, cropDimensions.x, cropDimensions.y)
-                                                .resize(processedImageSizes[imageSize].width)
-                                                .writeAsync('temp/uploads/'+image.fileNameBase+'_'+processedImageSizes[imageSize].width+'x'+processedImageSizes[imageSize].height+'.jpg')
-                                                .then(function(imageData) {
-                                                    params.Key = 'profile-photos/'+userData.id+'/background-photos/'+image.fileNameBase+'_'+processedImageSizes[imageSize].width+'x'+processedImageSizes[imageSize].height+'.jpg';
-                                                    params.Body = fs.createReadStream('temp/uploads/'+image.fileNameBase+'_'+processedImageSizes[imageSize].width+'x'+processedImageSizes[imageSize].height+'.jpg');
-                                                    return imageUploadS3.s3Upload(params)
-                                                        .then(function(uploadData) {
-                                                            // Success
-                                                        })
-                                                        .catch(function(error) {
-                                                            res.status(400).json('Error: '+error);
-                                                        })
-                                                })
-                                                .catch(function(error) {
-                                                    res.status(400).json('Error: '+error);
-                                                });
-                                        }
-                                        profilePhotoData[imageSize] = image.fileNameBase+'_'+processedImageSizes[imageSize].width+'x'+processedImageSizes[imageSize].height+'.'+image.extension;
-                                    }) (key)
-                                }
-                            }
-                            Profile.find({
-                                where: {userAccountId:uid}
-                            }).then ( function (profile) {
-                                profile.updateAttributes({
-                                    profileBackgroundPhoto: profilePhotoData
-                                });
-                                res.status(200).json({message:"USER_PROFILE_UPDATE_COMPLETE","data":profile});
-                            });
-                        })
-                        .catch(function(error) {
-                            res.status(400).json('Error: '+error);
-                        })
+            photoCtrl.imageUpload(req.body.imageFile, req.params.uid, processedImageSizes, 'background-photo').then(function(data) {
+                Profile.findOne({
+                    where: {userAccountId: req.params.uid}
+                }).then(function(profile) {
+                    profile.updateAttributes({
+                        profilePhoto: data
+                    }).then(function(newProfileData) {
+                        console.log(newProfileData);
+                        res.status(200).json({message:"USER_PROFILE_UPDATE_COMPLETE","data":newProfileData});
+                    });
                 })
-                .catch(function(error) {
-                    res.status(400).json('Error: '+error);
-                });
+            }).error(function(err) {
+                console.log(err);
+            });
+
         });
 
     /** Get the nearest town to the current user */
