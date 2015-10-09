1afec4b0e5e236d7f598c1edc25f6a69fce70985
Matt <developer@pioneerroad.com.au>
2015-10-09 13:32:48 +1100

    Updated sql for message active thread list

--- controllers/queries/listActiveThreads.sql
+++ controllers/queries/listActiveThreads.sql
@@ -1,7 +1,7 @@
 SELECT
 	DISTINCT ON ("message_threads"."threadId")
 	"message_threads"."threadId",
-	SELECT_FIRST("user_profiles"."userAccountId") as subscriberId,
+	SELECT_FIRST("user_profiles"."userAccountId") as "subscriberId",
 	SELECT_FIRST("user_profiles"."nickName") as subscriber_nickname,
 	string_agg("user_profiles"."nickName", ', ') as subscriber_list,
 	COUNT("message_user_threads"."userThreadId") as qty,
@@ -13,6 +13,8 @@ FROM "message_threads"
 	LEFT JOIN "message_user_threads" ON "message_user_threads"."threadId" = "message_threads"."threadId"
 	LEFT JOIN "user_profiles" ON "message_user_threads"."userAccountId" = "user_profiles"."userAccountId"
 WHERE "message_threads"."threadId"
-	IN (SELECT "message_user_threads"."threadId" FROM "message_user_threads" WHERE "message_user_threads"."userAccountId" = :uid)
-	AND "message_user_threads"."status" = 'active'
+	IN (SELECT "message_user_threads"."threadId" FROM "message_user_threads" WHERE "message_user_threads"."userAccountId" = :uid
+		AND "message_user_threads"."status" = 'active'
+	)
+
 GROUP BY "message_threads"."threadId";
