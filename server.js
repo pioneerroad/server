c5f9a224376c23650a62f75e611d6c33a447c04b
Matt <developer@pioneerroad.com.au>
2015-10-20 13:47:49 +1100

    Refactors for SSL

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var router  = express.Router();
 var app = express();
 var http = require('http');
 var https = require('https');
-var server = http.Server(app);
+var serverConfig = require(__dirname + '/config/serverConfig');
+var options = serverConfig.options;
+var server = https.createServer(options, app);
 var io = require('socket.io')(server); app.io = io; // Add io to app, to make it available to express routes.
 var cors = require('cors');
 var bodyParser = require('body-parser');
@@ -75,11 +77,10 @@ var ports = serverConfig.ports;
 /**
  * Synchronise models and launch server
  * */
-
 models.sequelize.sync().then(function () {
-    var modelSync = require(__dirname + '/migrations/sync_models') (models); // Apply non-sequelizeable elements to tables
+    //var modelSync = require(__dirname + '/migrations/sync_models') (models); // Apply non-sequelizeable elements to tables
 
-    server.listen(ports.noSSLPort, function() {
-        console.log('Listening on http://localhost:'+ports.noSSLPort);
+    server.listen(ports.SSLPort, function() {
+        console.log('Listening on http://localhost:'+ports.SSLPort);
     });
 });
